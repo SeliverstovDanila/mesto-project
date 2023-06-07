e9d2eb7672537db15a06f58472014b7814286cb8
SeliverstovDanila <Catsvill99@yandex.ru>
2023-06-07 10:47:48 +0400

    Update 2.1

--- script.js
+++ script.js
@@ -1,156 +1,182 @@
 const initialCards = [
   {
-    name: 'End of journey...',
-    link: 'https://images.unsplash.com/photo-1580668095433-a1ad0a985391?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
+    name: 'Что это?',
+    link: 'https://images.unsplash.com/photo-1598188306155-25e400eb5078?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80',
+    alt: 'Страшное...'
   },
   {
     name: 'Скоро вернусь',
-    link: 'https://images.unsplash.com/photo-1493406300581-484b937cdc41?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
+    link: 'https://images.unsplash.com/photo-1493406300581-484b937cdc41?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    alt: 'Скоро'
   },
   {
     name: 'Угадай где я?',
-    link: 'https://images.unsplash.com/photo-1570458436416-b8fcccfe883f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80'
+    link: 'https://images.unsplash.com/photo-1570458436416-b8fcccfe883f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+    alt: 'А где ты?'
   },
   {
     name: 'Земля в иллюминаторе',
-    link: 'https://images.unsplash.com/photo-1527150602-a98f7a6f2746?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80'
+    link: 'https://images.unsplash.com/photo-1527150602-a98f7a6f2746?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80',
+    alt: 'Отпусти'
   },
   {
     name: 'Минимализм...',
-    link: 'https://images.unsplash.com/photo-1617617495223-ed838eaa20e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=685&q=80'
+    link: 'https://images.unsplash.com/photo-1617617495223-ed838eaa20e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=685&q=80',
+    alt: 'это модно'
   },
   {
-    name: 'Что это?',
-    link: 'https://images.unsplash.com/photo-1598188306155-25e400eb5078?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80'
+    name: 'End of journey...',
+    link: 'https://images.unsplash.com/photo-1580668095433-a1ad0a985391?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
+    alt: 'Конец путешествия'
   }
 ];
-
-const menu = document.querySelector('.menu');
-const cardBlock = menu.querySelector('.elements');
-const cardsContainer = menu.querySelector('.element'); //Место для карточек
-
-const profile = document.querySelector('.profile');
-
-
-const popupEdit = document.querySelector('#modal-profile'); // Модальное окно - редактировать профиль
-const popupAdd = document.querySelector('#modal-card'); // Модальное окно - добавить карточку
-const popupZoom = document.querySelector('#photo-zoom'); // Модальное окно - увеличить фото
-const buttonPhoto = document.querySelector('.element__photo');
-const buttonEdit = document.querySelector('.profile__editbutton'); //Кнопка - редактировать профиль
-const buttonAdd = document.querySelector('.profile__addbutton'); //Кнопка - добавить карточку
-const buttonClose = document.querySelector('.popup__button-close'); //Кнопка - закрыть редактирование профиля
-const buttonAddFormClose = document.querySelector('#close-modal'); //Кнопка - закрыть форму добавления карточки
-const buttonZoomClose = document.querySelector('#close-zoom'); //Кнопка - закрыть форму увеличения фото
-
-//функция открытия попапа
-function popupOpen(modal) {
-  modal.classList.add('popup_open');
-}
-
-//функция закрытия попапа
-function closeModal(modal) {
-  modal.classList.remove('popup_open');
-}
-
-//открытие попапа редактировать профиль
-function openModal(modal) {
-  popupOpen(modal);
-}
-
-const modalImage = document.querySelector('.popup__image');
-const modalTitle = document.querySelector('.popup__heading');
-//функция открытия попапа добавления фото
-function openModalAddCard(modal) {
-  popupOpen(modal);
-}
-
-function openZoom(modal) {
-  popupOpen(modal);
-}
-
-buttonEdit.addEventListener('click', () => { openModal(popupEdit) });
-buttonAdd.addEventListener('click', () => { openModalAddCard(popupAdd) });
-popupZoom.addEventListener('click', () => { openZoom(popupZoom) });
-buttonClose.addEventListener('click', () => { closeModal(popupEdit) });
-buttonAddFormClose.addEventListener('click', () => { closeModal(popupAdd) });
-
-const formElement = document.querySelector('.popup__form-container'); // Модальное окно(редактировать профиль)
-const profileTitle = document.querySelector('.profile__title'); // Имя пользователя(на странице)
-const profileSubtitle = document.querySelector('.profile__subtitle');
-const nameInput = document.querySelector('#profile__name'); //Имя пользователя(input)
+//Редактировать профиль
+const buttonEdit = document.querySelector('.profile__editbutton');
+const modalProfile = document.querySelector('#modal-profile');
+const modalForm = document.querySelector('.popup__form-container');
+const nameInput = document.querySelector('#profile__name');
 const jobInput = document.querySelector('#profile__info');
+const profileClose = document.querySelector('#close-profile');
+const profileContainer = document.querySelector('.profile__info');
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+// Создать карточку
+const container = document.querySelector('.element');
+const template = document.querySelector('#elements').content;
+// Модальное окно - добавить карточку
+const profileModalAdd = document.querySelector('.profile__addbutton');
+const modalAdd = document.querySelector('#modal-card');
+const newCardName = document.querySelector("#card__addname");
+const inputForm = document.querySelector('#add-form');
+const newPhotoLink = document.querySelector("#card__url");
+const addForm = document.querySelector('#button-create');
+const addClose = document.querySelector('#close-modal');
 
-//Редактирвание профиля пользователя
-function handleFormSubmit(evt) {
-  evt.preventDefault();
+//Увеличить фото
+const modalZoom = document.querySelector('#photo-zoom');
+const closeZoom = document.querySelector('#close-zoom');
+const zoomImage = modalZoom.querySelector('.popup__image');
+const zoomHeading = modalZoom.querySelector('.popup__heading');
+
+function Default(){
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileSubtitle.textContent;
+}
+
+function openModal(open) {
+  open.classList.add('popup_open');
+}
+
+function closeModal(close) {
+  close.classList.remove('popup_open');
+}
+// Новая карточка
+function addCard(userText) {
+  const newCard = template.querySelector('.element__cards').cloneNode(true);
+  newCard.querySelector('.element__photo').src = userText.link;
+  newCard.querySelector('.element__photo').alt = userText.alt;
+  newCard.querySelector('.element__title').textContent = userText.name;
+  newCard.querySelector('.element__like').addEventListener('click', activeLike);
+  newCard.querySelector('.element__delite').addEventListener('click', removeCard);
+  newCard.querySelector('.element__photo').addEventListener('click', openZoom);
+  return newCard
+}
+
+function createCards(create) {
+  const add = document.createDocumentFragment();
+
+  create.forEach(el => {
+      const newCard = addCard(el);
+      add.append(newCard);
+  })
+  return add
+}
+
+function like(e) {
+  e.classList.toggle('element__like_active');
+}
+// Редактировать профиль
+function openModalProfile() {
+  openModal(modalProfile);
+  Default()
+}
+
+function closeModalProfile() {
+  closeModal(modalProfile);
+  modalForm.reset()
+}
+
+function handleProfile(e) {
+  e.preventDefault();
+
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
-  closeModal(popupEdit);
+
+  closeModalProfile();
+}
+// Модальное окно - добавить карточку
+function handleAddModal() {
+  openModal(modalAdd);
 }
 
-formElement.addEventListener('submit', handleFormSubmit); // Сохранить форму
+function handleAddClose() {
+  closeModal(modalAdd);
+  inputForm.reset();
+}
 
-const cardAddName = document.querySelector('#card__addname');
-const inputUrl = document.querySelector('#card__url');
-//Редактирвание профиля пользователя
-function handleAddFormSubmit(evt) {
+function handleAdd(evt) {
   evt.preventDefault();
-  profileTitle.textContent = cardAddName.value;
-  profileSubtitle.textContent = inputUrl.value;
-  closeModal(popupAdd);
+
+  const cardDefault = {
+      link: newPhotoLink.value,
+      name: newCardName.value,
+      alt: newCardName.value
+  }
+
+  const formCard = addCard(cardDefault);
+  container.prepend(formCard);
+
+  handleAddClose();
 }
 
-// formElement.addEventListener('submit', handleAddFormSubmit); // Сохранить форму добавления карточки
-
-const elementContainer = document.querySelector('.element');
-const instalElements = document.querySelector('#elements').content;
-
-
- const cardName = initialCards.map(item => item.name);
- const cardLink = initialCards.map(item => item.link);
-
- const userInfo = initialCards.map(function (item) {
-   return {
-     name: item.name,
-     link: item.link
-   };
- });
-
- function render() {
-   userInfo.forEach(renderCard);
- }
-
-function renderCard({ name, link }) {
-  const infoElement = instalElements
-    .querySelector('.element__cards')
-    .cloneNode(true);
-  infoElement.querySelector('.element__title').textContent = name;
-  infoElement.querySelector('.element__photo').src = link;
-
-  elementContainer.prepend(infoElement);
+function cardList() {
+  const form = createCards(initialCards);
+  container.append(form);
 }
 
-render();
+function removeCard(evt) {
+  const deliteCard = evt.target.closest('.element__cards');
+  deliteCard.remove();
+}
 
-// like card
-document.querySelectorAll('.element__like').forEach(
-  el => el.addEventListener("click", function (e) {
-    this.classList.toggle("element__like_active");
-  })
-);
+function activeLike(evt) {
+  const toogle = evt.target;
+  like(toogle);
+}
+// Увеличить фото
+function openZoom(evt) {
+  const deliteCard = evt.target.closest('.element__cards');
+  const image = deliteCard.querySelector('.element__photo');
+  const zoomImageName = deliteCard.querySelector('.element__title');
+  openModal(modalZoom);
+  zoomImage.src = image.src;
+  zoomImage.alt = image.alt;
+  zoomHeading.textContent = zoomImageName.textContent;
+}
 
-// delite card
-  const deleteButton = document.querySelector('#remove');
+function closeModalZoom() {
+  closeModal(modalZoom);
+}
 
-  deleteButton.addEventListener('click', function () {
-    const cardItem = deleteButton.closest('.element__cards');
-    cardItem.remove();
-  })
+cardList();
 
-  const cardDelete = function(event) {
-    if (event.target.classList.contains('place-card__delete-icon')) {
-        const eventTarget = event.target;
-        const deleteCard = eventTarget.closest('.place-card');
-        deleteCard.parentNode.removeChild(deleteCard);
-    }
-};
+// Модальное окно - профиль
+buttonEdit.addEventListener('click', openModalProfile);
+profileClose.addEventListener('click', closeModalProfile);
+modalForm.addEventListener('submit', handleProfile);
+// Модальное окно - сохранить карточку
+profileModalAdd.addEventListener('click', handleAddModal);
+addClose.addEventListener('click', handleAddClose);
+addForm.addEventListener('submit', handleAdd);
+// Закрыть увеличение фото
+closeZoom.addEventListener('click', closeModalZoom);
